fc2ae6d5e68fb0017b016c8a67fe0ec91e40e9b1
Ilya_Spasskiy <spasskiy.iv@yandex.ru>
2023-04-17 11:29:30 +0300

    Add new function - Details

--- school_dance/src/main/java/com/ilsy/school_dance/controllers/BlogController.java
+++ school_dance/src/main/java/com/ilsy/school_dance/controllers/BlogController.java
@@ -1,17 +1,62 @@
 package com.ilsy.school_dance.controllers;
 
+import com.ilsy.school_dance.models.Post;
+import com.ilsy.school_dance.repo.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.Optional;
 
 @Controller
 public class BlogController {
+
+
     @GetMapping("/blog")
     public String blog(Model model) {
+        Iterable<Post> posts = postRepository.findAll();
+        model.addAttribute("posts", posts);
         return "blog";
     }
+
+
+
+    @Autowired
+    private PostRepository postRepository;
+
     @GetMapping("/blog/add")
     public String blogAdd(Model model) {
         return "blogAdd";
     }
+
+    @PostMapping("/blog/add")
+    public String blogAddPost(@RequestParam String title,
+                              @RequestParam String anons,
+                              @RequestParam String date,
+                              @RequestParam String text,
+                              Model model) {
+        Post post = new Post(title, anons, date, text);
+        postRepository.save(post);
+        return "redirect:/blog";
+    }
+
+
+
+    @GetMapping("/blog/{id}")
+    public String blogDetails(@PathVariable(value = "id") long id, Model model) {
+        if (!postRepository.existsById(id)) {
+            return "redirect:/blog";
+        }
+        Optional<Post> post = postRepository.findById(id);
+        ArrayList<Post> res = new ArrayList<>();
+        post.ifPresent(res::add);
+        model.addAttribute("post", res);
+        return "blogDetails";
+    }
+
 }
